d424693c45d5e5125012d8240b0f1bbf3b251a11
Elina44 <103180214+Elina44@users.noreply.github.com>
2022-10-19 15:47:27 +0200

    Création delete album

--- src/api/albums/delete.js
+++ src/api/albums/delete.js
@@ -3,16 +3,16 @@ const database = require("../../database");
 module.exports = (req, res) => {
   // your code here !
   database
-    .delete("select * from album where id = ?", [id])
+    .query("delete from album where id = ?", [id])
     .then(([result]) => {
       if (result.affectedRows === 0) {
-        res.sendStatus(404);
+        res.sendStatus(404).send("Not found");
       } else {
         res.sendStatus(204);
       }
     })
     .catch((err) => {
       console.error(err);
-      res.sendStatus(500);
+      res.status(500).send("Error deleting the album");
     });
 };
