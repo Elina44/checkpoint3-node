1dc3f8f957dc352d29c1c64c6c062a6b8562abfc
Elina44 <103180214+Elina44@users.noreply.github.com>
2022-10-19 16:22:49 +0200

    Complément de la requête dans la jointure

--- src/api/albums/getTracks.js
+++ src/api/albums/getTracks.js
@@ -3,7 +3,7 @@ const database = require("../../database");
 module.exports = (req, res) => {
   // your code here !
   database
-  .query("select a.title, a.genre, t.title from album a inner join track t on a.id = t.id_album")
+  .query("select a.title, a.genre, a.picture, a.artiste t.title, t.tyoutube_url, t.id_album from album a inner join track t on a.id = t.id_album")
   .then(([album]) => {
     res.json(album);
   })
