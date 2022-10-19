db3a88bfb344915d673df9d5d61556a690a7791a
Elina44 <103180214+Elina44@users.noreply.github.com>
2022-10-19 16:15:28 +0200

    Ajout getTracks

--- src/api/albums/getTracks.js
+++ src/api/albums/getTracks.js
@@ -1,4 +1,14 @@
+const database = require("../../database");
+
 module.exports = (req, res) => {
   // your code here !
-  
+  database
+  .query("select a.title, a.genre, t.title from album a inner join track t on a.id = t.id_album")
+  .then(([album]) => {
+    res.json(album);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send("Error retrieving data from database");
+  }); 
 };
