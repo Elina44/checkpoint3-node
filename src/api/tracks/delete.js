297dd90f982d4530c060d5cbd4ee5de685a4c40b
Elina44 <103180214+Elina44@users.noreply.github.com>
2022-10-19 16:02:55 +0200

    Création pour track

--- src/api/tracks/delete.js
+++ src/api/tracks/delete.js
@@ -1,3 +1,20 @@
+const database = require("../../database");
+
 module.exports = (req, res) => {
   // your code here !
+  database
+    .query("delete from track where id = ?", [id])
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404).send("Not found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error deleting the track");
+    });
 };
+
+
